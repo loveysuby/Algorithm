3d71c92f178b1ba837e85b8466d95c8072149d64
loveysuby <crad_on25@khu.ac.kr>
2020-12-27 15:39:39 +0900

    그대로 출력하기(11718)

---
+++ BOJ/11718.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define MAX_LINE 100
+int main()
+{
+    //정적 배열로 입력받을 수는 없겠다 -> STL 어쩔 수 없이 사용
+    string str;
+    for (int i = 0; i < MAX_LINE; i++)
+    {
+        getline(cin, str);
+        cout << str << "\n";
+    }
+    return 0;
+}
