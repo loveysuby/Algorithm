5c73283a855e752fa569b1ddefa14e66213f31b1
loveysuby <crad_on25@khu.ac.kr>
2020-12-28 14:58:32 +0900

    기찍 N(2742)

---
+++ BOJ/2742.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//가급적 endl 대신 \n으로,, endl : output buffer를 시행마다 초기화시켜줌 >> \n 출력보다 느리다.
+int main()
+{
+    int count;
+    std::cin >> count;
+    for (int i = count; i >= 1; i--)
+        std::cout << i << "\n";
+    return 0;
+}
