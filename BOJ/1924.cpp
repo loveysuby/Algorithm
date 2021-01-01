126f27fc06352d638e9b8d872f725480a369a1b4
loveysuby <crad_on25@khu.ac.kr>
2021-01-01 15:47:42 +0900

    1924

--- BOJ/1924.cpp
+++ BOJ/1924.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 int main()
 {
-    int a = 10;
-    int b = 24;
-    int c;
-    printf("successed");
-    c = a + b;
+    int daysAtMonth[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    string daysOnWeek[7] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+    int month, day;
+    cin >> month >> day;
+    //월별로 저장해둔 요일 수를 for로 접근, input day값에 더해 총 일수 구함
+    for (int i = 1; i < month; i++)
+        day += daysAtMonth[i];
+    //day % 7 의 value == 일요일(0) 부터 시작되는 string 배열의 index
+    cout << daysOnWeek[day % 7];
+
     return 0;
 }
