2760d0c336974eef0faf076e01d6774d5861edb9
loveysuby <crad_on25@khu.ac.kr>
2021-01-03 00:45:19 +0900

    1로 만들기(1463)

--- BOJ/1463.cpp
+++ BOJ/1463.cpp
@@ -10,17 +10,19 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
     int num;
     cin >> num;
     //결과값에서는 필요한 연산 수가 없다.
-    memset(dp, 0, sizeof(dp) * (num + 1));
+    memset(dp, 0, sizeof(int) * (num + 1));
 
     for (int i = 2; i <= num; ++i)
     {
-        dp[i] = dp[i - 1] + 1;
-        dp[i] = dp[i / 3] + 1;
-        dp[i] = dp[i / 2] + 1;
+        dp[i] = dp[i - 1] + 1; //1 빼주기
+        if (i % 2 == 0)
+            dp[i] = min(dp[i], dp[i / 2] + 1); // 바로 2 나눈거랑 1뺀거 (기존) 중에 더 작은 연산수 dp배열에 저장
+        if (i % 3 == 0)
+            dp[i] = min(dp[i], dp[i / 3] + 1); // ``
     }
+    cout << dp[num];
     return 0;
 }
