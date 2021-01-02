55f28118bde572acd06bbb1bc38e9cd378b29fda
loveysuby <crad_on25@khu.ac.kr>
2021-01-03 00:14:58 +0900

    1463

--- BOJ/1463.cpp
+++ BOJ/1463.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 using namespace std;
+
+int dp[1000001];
 
 int main()
 {
@@ -8,5 +11,16 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int num;
+    cin >> num;
+    //결과값에서는 필요한 연산 수가 없다.
+    memset(dp, 0, sizeof(dp) * (num + 1));
+
+    for (int i = 2; i <= num; ++i)
+    {
+        dp[i] = dp[i - 1] + 1;
+        dp[i] = dp[i / 3] + 1;
+        dp[i] = dp[i / 2] + 1;
+    }
     return 0;
 }
