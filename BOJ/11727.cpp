f74e516e9db6c796c817018a1951115f8e17ff65
loveysuby <crad_on25@khu.ac.kr>
2021-01-04 16:36:01 +0900

    11727

---
+++ BOJ/11727.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    int dp[1001];
+
+    return 0;
+}
