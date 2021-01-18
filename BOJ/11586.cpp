0bf97e98b4241cf8cefe5d7e2bb216be1223447a
loveysuby <crad_on25@khu.ac.kr>
2021-01-18 23:19:43 +0900

    지영 공주님의 마법 거울(11586)

---
+++ BOJ/11586.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int size;
+    cin >> size;
+    cin.ignore();
+    vector<string> mirrorStack;
+
+    for (int i = 0; i < size; i++)
+    {
+        string faceInput;
+        getline(cin, faceInput);
+        mirrorStack.push_back(faceInput);
+    }
+
+    int status;
+    cin >> status;
+    switch (status)
+    {
+    case 1:
+        for (int i = 0; i < size; i++)
+            cout << mirrorStack[i] << '\n';
+        break;
+    case 2:
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = size - 1; j >= 0; j--)
+                cout << mirrorStack[i][j];
+            cout << '\n';
+        }
+        break;
+    case 3:
+        for (int i = size - 1; i >= 0; i--)
+            cout << mirrorStack[i] << '\n';
+        break;
+    }
+}
