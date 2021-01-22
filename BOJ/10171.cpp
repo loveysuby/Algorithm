63649d5f2c425879aa4720ee6ba3d978fa804cc3
loveysuby <crad_on25@khu.ac.kr>
2021-01-22 23:13:04 +0900

    고양이(10171)

---
+++ BOJ/10171.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+    cout << "\\    /\\" << '\n';
+    cout << " )  ( ')" << '\n';
+    cout << "(  /  )" << '\n';
+    cout << " \\(__)|" << '\n';
+    return 0;
+}
