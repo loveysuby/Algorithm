990d37540c0bd190007b0249123e133a3817481e
loveysuby <crad_on25@khu.ac.kr>
2021-02-01 00:04:37 +0900

    교수님 그림이 깨지는데요?(20205)

--- BOJ/20205.cpp
+++ BOJ/20205.cpp
@@ -4,15 +4,8 @@ using namespace std;
 
 bool pixel[105][105];
 
-int main()
+void calcPixel(int n, int k)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int n, k;
-    cin >> n >> k;
-
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -24,16 +17,27 @@ int main()
                     pixel[x][y] = chk;
         }
     }
-
+}
+void displayPixel(int n, int k)
+{
     for (int i = 0; i < n * k; i++)
     {
         for (int j = 0; j < n * k; j++)
-        {
             cout << pixel[i][j] << ' ';
-        }
-
         cout << '\n';
     }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+
+    calcPixel(n, k);
+    displayPixel(n, k);
 
     return 0;
 }
