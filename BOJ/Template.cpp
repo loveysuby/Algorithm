5e34f3dfc93c5b99fb56c44916419fa5337d9e3a
‍송효섭[학생](소프트웨어융합대학 컴퓨터공학과) <crad_on25@khu.ac.kr>
2021-04-16 11:49:00 +0000

    UPDATE: PS Template(C++)

---
+++ BOJ/Template.cpp
@@ -0,0 +1,19 @@
+//Header
+#include <iostream>
+//Define
+#define ll long long
+
+using namespace std;
+
+//Global elements
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	
+    return 0;
+}
