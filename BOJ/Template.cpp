9b9a3d039822464b9425063e8c5117b402cafc83
‍송효섭[학생](소프트웨어융합대학 컴퓨터공학과) <crad_on25@khu.ac.kr>
2021-04-27 13:54:13 +0000

    UPDATE: modified template.cpp env

--- BOJ/Template.cpp
+++ BOJ/Template.cpp
@@ -1,19 +1,20 @@
 //Header
 #include <iostream>
 //Define
-#define ll long long
-
 using namespace std;
 
 //Global elements
 
 
+//Function
 
+
+//Driver
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+	cout.tie(NULL);
 	
     return 0;
 }
