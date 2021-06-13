2109b29e465fa40bc16c0c793f823680085807cb
‍송효섭[학생](소프트웨어융합대학 컴퓨터공학과) <crad_on25@khu.ac.kr>
2021-06-13 13:25:43 +0000

    boj-10998,11021,11022

---
+++ BOJ/11021.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int cnt;
+	cin >> cnt;
+	int num1, num2;
+	for (int i = 1; i <= cnt; i++) {
+		cin >> num1 >> num2;
+		cout << "Case #" << i << ": " << num1 + num2 << endl;
+	}
+	return 0;
+}
