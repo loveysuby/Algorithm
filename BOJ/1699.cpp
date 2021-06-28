ff220dc1936f7da8f4cf1bee221666de4a7eb5e6
‍송효섭[학생](소프트웨어융합대학 컴퓨터공학과) <crad_on25@khu.ac.kr>
2021-06-28 13:53:43 +0000

    boj-1699

--- BOJ/1699.cpp
+++ BOJ/1699.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(void) {
-	
-	return 0;
-	
+int main()
+{
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int dp[100001];
+	int N;
+	cin>>N;
+	for(int idx=0;idx<=N;idx++)	dp[idx] = idx;	
+	for(int i=4;i<=N;i++)
+		for(int j=2;j*j<=i;j++)
+			dp[i] = min(dp[i],dp[i-j*j]+1);
+	cout<<dp[N];
+    return 0;
 }
