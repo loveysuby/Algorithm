44b35c70f37bd8680d51c7da76653577a0be32dc
‍송효섭[학생](소프트웨어융합대학 컴퓨터공학과) <crad_on25@khu.ac.kr>
2021-07-02 14:59:40 +0000

    boj-1330

---
+++ BOJ/1330.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Define
+
+//Global elements
+
+//Function
+
+//Driver
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int a,b; cin>>a>>b;
+	if(a>b) cout<<">";	
+	else if(a==b) cout<<"==";	
+	else cout<<"<";
+	
+    return 0;
+}
