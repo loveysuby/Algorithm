8e67c00d4ff1d8d6e887e79e1d9c97c1854953c2
‍송효섭[학생](소프트웨어융합대학 컴퓨터공학과) <crad_on25@khu.ac.kr>
2022-01-02 09:42:43 +0000

    boj-11651: operator method override

---
+++ BOJ/11651.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pii pair<int,int>
+bool compare(pii a, pii b){
+	if(a.second == b.second)
+		return a.first < b.first;
+	else
+		return a.second < b.second;
+}
+int main(){
+	fastio;
+	vector<pii> v;
+	int N; cin>>N;
+	for(int i=0;i<N;i++){
+		int tmp1,tmp2; cin>>tmp1>>tmp2;
+		v.push_back({tmp1,tmp2});
+	}
+	sort(v.begin(),v.end(),compare);
+	for(auto i : v)
+		cout<<i.first<<' '<<i.second<<'\n';
+}
