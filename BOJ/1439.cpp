b8a86426fa7e0425e80f1cca887bfbceedf23ef9
‍송효섭[학생](소프트웨어융합대학 컴퓨터공학과) <crad_on25@khu.ac.kr>
2022-01-22 14:56:08 +0000

    boj-1439: greedy

---
+++ BOJ/1439.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define MAX 1000001
+int main(){
+	fastio;
+	int arr[MAX];
+	string s; cin>>s;
+	int res = 0;
+	//0, 1 바뀌는 포인트 개수 / 2 = 정답
+	for(int i=1;i<s.size();i++){
+		if(s[i] != s[i-1])
+			res++;
+	}
+    //답 1일때 보정
+	cout<<(res+1)/2;
+}
