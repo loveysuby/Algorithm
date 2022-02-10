e2174740aed28b6d5ab160ad7100b32e2f5c9e88
‍송효섭[학생](소프트웨어융합대학 컴퓨터공학과) <crad_on25@khu.ac.kr>
2022-02-10 14:57:19 +0000

    boj-18108: basic print format examples

---
+++ BOJ/18108.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+	fastio;
+	int y; cin>>y;
+	cout<<y-543;
+}
