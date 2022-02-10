98e40ca66eb80ac310d591bbbe6fade3e21d79b3
‍송효섭[학생](소프트웨어융합대학 컴퓨터공학과) <crad_on25@khu.ac.kr>
2022-02-10 14:55:26 +0000

    boj-10950,8393,15552: basic operator examples

---
+++ BOJ/15552.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+	fastio;
+	int T; cin>>T;
+	while(T--){
+		int a,b; cin>>a>>b;
+		cout<<a+b<<'\n';
+	}
+}
---
+++ BOJ/8393.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+	fastio;
+	int T; cin>>T;
+	cout<<(T*(T+1))/2;
+}
