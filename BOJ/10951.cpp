f6a4d5d34ac5f77a6d08736983f8ec66ecbe4432
‍송효섭[학생](소프트웨어융합대학 컴퓨터공학과) <crad_on25@khu.ac.kr>
2022-02-17 10:06:03 +0000

    boj-10951,10952: while loop basic example

---
+++ BOJ/10951.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int x,y;
+	while(scanf("%d %d", &x,&y) != EOF)
+		printf("%d\n", x+y);
+}
---
+++ BOJ/10952.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int x,y;
+	while(scanf("%d %d", &x,&y)){
+		if(!x && !y) break;
+		printf("%d\n", x+y);
+	}
+}
