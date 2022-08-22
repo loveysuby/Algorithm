86797c6fc735951a85e564e10258b9aea9232c2b
loveysuby <crad_on25@naver.com>
2022-08-22 23:56:30 +0900

    boj-15654: backtracking

--- BOJ/15654.cpp
+++ BOJ/15654.cpp
@@ -1,32 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-int N,M;
-vector<int> v,r;
-bool isChk[8];
-void dfs(int cnt){
-	if(cnt == M){
-		for(int i = 0; i<r.size();i++)
-			cout<<r[i]<<' ';
-		cout<<"\n";
-		return;
-	}
-	for(int i=0;i<N;i++){
-		if(isChk[i]) continue;
-		isChk[i] = true;
-		r.push_back(v[i]);
-		dfs(cnt+1);
-		r.pop_back();
-		isChk[i] = false;
-	}	
+int N, M;
+vector<int> v, r;
+bool isChk[9];
+void dfs(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < r.size(); i++)
+            cout << r[i] << ' ';
+        cout << "\n";
+        return;
+    }
+    for (int i = 0; i < N; i++) {
+        if (isChk[i]) continue;
+        isChk[i] = true;
+        r.push_back(v[i]);
+        dfs(cnt + 1);
+        r.pop_back();
+        isChk[i] = false;
+    }
 }
-int main(){
-	fastio;
-	cin>>N>>M;
-	for(int i=0;i<N;i++) {
-		int tmp; cin>>tmp;
-		v.push_back(tmp);
-	}
-	sort(v.begin(),v.end());
-	dfs(0);
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        int tmp;
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    sort(v.begin(), v.end());
+    dfs(0);
 }
