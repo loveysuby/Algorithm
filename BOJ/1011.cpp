9f52f92badcbe84ec2c9a68ad8269d089eef157b
Hyoseop Song <crad_on25@naver.com>
2023-07-27 23:48:59 +0900

    boj-1181: sort method

--- BOJ/1011.cpp
+++ BOJ/1011.cpp
@@ -1,23 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define fastio                   \
+	ios::sync_with_stdio(false); \
+	cin.tie(NULL);               \
+	cout.tie(NULL);
 #define ll long long
-int main(){
+int main()
+{
 	fastio;
-	int T; cin>>T;
-	while(T--){
+	int T;
+	cin >> T;
+	while (T--)
+	{
 		ll st, end, i = 1;
 		cin >> st >> end;
 		ll dist = end - st;
-		while(1){
-			//{(N-1)^2 + N} ~ N^2 (이전 제곱수와의 중간값< dist <제곱수) 
-			if(dist<=i*i){
-				cout<<i*2 - 1 <<'\n';
+		while (1)
+		{
+			//{(N-1)^2 + N} ~ N^2 (이전 제곱수와의 중간값< dist <제곱수)
+			if (dist <= i * i)
+			{
+				cout << i * 2 - 1 << '\n';
 				break;
 			}
-			//N^2  ~  {N^2 ~ N^2 + N} (제곱수< dist <다음 제곱수와의 중간값) 
-			else if (dist<= i*i + i){
-				cout<< i * 2 <<'\n';
+			// N^2  ~  {N^2 ~ N^2 + N} (제곱수< dist <다음 제곱수와의 중간값)
+			else if (dist <= i * i + i)
+			{
+				cout << i * 2 << '\n';
 				break;
 			}
 			i++;
--- BOJ/1181.cpp
+++ BOJ/1181.cpp
@@ -1,25 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-bool cmp(string a, string b){
-	if(a.length() == b.length()){
-		return a<b;
+#define fastio                   \
+	ios::sync_with_stdio(false); \
+	cin.tie(NULL);               \
+	cout.tie(NULL);
+bool cmp(string a, string b)
+{
+	if (a.length() == b.length())
+	{
+		return a < b;
 	}
 	return a.length() < b.length();
 }
-int main(){
+int main()
+{
 	fastio;
-	int N; cin>>N;
-	
+	int N;
+	cin >> N;
+
 	vector<string> v(N);
-	
-	for(int i=0;i<N;i++)
-		cin>>v[i];
-	
-	sort(v.begin(),v.end(),cmp);
-	
-	v.erase(unique(v.begin(),v.end()),v.end());
-	
-	for(int i=0;i<v.size();i++)
-		cout<<v[i]<<endl;
+
+	for (int i = 0; i < N; i++)
+		cin >> v[i];
+
+	sort(v.begin(), v.end(), cmp);
+
+	v.erase(unique(v.begin(), v.end()), v.end());
+
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << endl;
 }
