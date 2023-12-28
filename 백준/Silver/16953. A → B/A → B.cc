a4d059de5f9752c38f5a07fdc6aee1aba8cc8938
Hyoseop Song <crad_on25@naver.com>
2023-12-28 22:40:30 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2284 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define pli pair<long long, int>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    queue<pli> q;
+    int a, b;
+    cin >> a >> b;
+    q.push(make_pair(a, 0));
+    while (!q.empty())
+    {
+        pli c = q.front();
+        q.pop();
+        if (c.first == b)
+        {
+            cout << c.second + 1;
+            return 0;
+        }
+        if (c.first * 10 + 1 <= b)
+        {
+            q.push(make_pair(c.first * 10 + 1, c.second + 1));
+        }
+        if (c.first * 2 <= b)
+        {
+            q.push(make_pair(c.first * 2, c.second + 1));
+        }
+    }
+    cout << -1;
+    return 0;
+}
