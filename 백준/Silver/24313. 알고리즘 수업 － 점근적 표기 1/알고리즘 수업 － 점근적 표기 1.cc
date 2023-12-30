a34a026a23f8b7f6170bb6331203ffdc4bc86d03
Hyoseop Song <crad_on25@naver.com>
2023-12-30 23:56:33 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -1,20 +1,11 @@
 #include <iostream>
 
 using namespace std;
-int solve(int a0, int a1, int c, int n0)
-{
-    // a1 * x + a0 <= c * n => (c - a1) * n >= a0 (On 만족하는 n의 최소값)
-    int exponent = c - a1;
-    int term = exponent * n0 - a0; // 최소값에 의한 term
-    return exponent < 0 || term < 0 ? 0 : 1;
-}
 
 int main()
 {
     int a0, a1, c, n0;
-    cin >> a1 >> a0;
-    cin >> c;
-    cin >> n0;
-    cout << solve(a0, a1, c, n0);
+    cin >> a1 >> a0 >> c >> n0;
+    cout << ((c - a1) < 0 || (c - a1) * n0 - a0 < 0 ? 0 : 1);
     return 0;
 }
