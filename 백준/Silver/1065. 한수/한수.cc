5557f430fa9c99841f7c58768e51fe96c0cf21ab
Hyoseop Song <crad_on25@naver.com>
2024-01-01 00:00:18 +0900

    [Silver IV] Title: 한수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main()
+{
+    fastio;
+    int N, R = 0;
+    cin >> N;
+    int *arr = new int[N + 1];
+    for (int i = 1; i <= N; i++)
+    {
+        if (i < 100)
+        {
+            R++;
+            continue;
+        }
+        else if (i / 100 - (i % 100) / 10 ==
+                 (i % 100) / 10 - (i % 100) % 10)
+            R++;
+    }
+    cout << R;
+    return 0;
+}
