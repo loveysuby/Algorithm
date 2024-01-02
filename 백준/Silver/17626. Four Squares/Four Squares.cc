4b9c787e0b09f9ab590129b783f95fc9a8a9d414
Hyoseop Song <crad_on25@naver.com>
2024-01-02 23:59:20 +0900

    [Silver III] Title: Four Squares, Time: 32 ms, Memory: 2276 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    vector<int> dp(N + 1, 1e9);
+    for (int i = 1; i <= N; i++)
+        if ((int)sqrt(i) * (int)sqrt(i) == i)
+            dp[i] = 1;
+    for (int i = 1; i <= N; i++)
+        for (int j = 1; j <= (int)sqrt(i); j++)
+            dp[i] = min(dp[i], dp[i - j * j] + dp[j * j]);
+    cout << dp[N];
+    return 0;
+}
